ccc7bb24d55835944fefb08372d411b500ffaf8e
Morten Noddeland <morten@noddeland.no>
2015-07-07 19:24:17 +0200

    Added python pip and virtualenv

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,3 +6,10 @@ ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 # Install Pygments (used for syntax highlighted 'cat')
 sudo easy_install Pygments
+
+# Installs Pip, a Package manager for Python
+sudo easy_install pip
+
+# Virtual environment for Django projects
+sudo pip install virtualenv
+
