ebfbacff86af55944f5f9acf437caa10fa27c6ff
Morten Noddeland <morten@noddeland.no>
2017-05-20 23:45:14 +0200

    Add bash files and a bash setup script

--- settings.sh
+++ settings.sh
@@ -8,23 +8,25 @@ GIT_EMAIL="morten@noddeland.no"
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
 BREW_CASKS="1password alfred arq atom balsamiq-mockups disk-inventory-x dropbox evernote flux gitx-rowanj google-chrome iconjar imageoptim iterm2 jetbrains-toolbox licecap opera postman postbox skype slack spotify steam sublime-text superduper teamviewer the-unarchiver transmission vlc wunderlist"
 
-# HYPER PACKAGES (installed with hpm)
-HPM_PACKAGES="hyper-snazzy hyperlinks hyperterm-1password"
-
-# NODE PACKAGES
+# NODE
 NPM_PACKAGES="n ncu yarn"
 
-# OPTIONAL BREW PACKAGES
-OPTIONAL_BREW_PACKAGES="pyenv python3 vim wget"
+# HYPER (installed with hpm)
+HPM_PACKAGES="hyper-snazzy hyperlinks hyperterm-1password"
 
+# BREW (OPTIONAL)
+OPTIONAL_BREW_PACKAGES="pyenv python3 vim wget"
 
 # --------------------------------------------------------------
 # DANGER ZONE! EDIT THE FOLLOWING SETTINGS WITH CARE
 # Removing some of them might break the setup :/
 # --------------------------------------------------------------
 
-# ESSENTIAL BREW PACKAGES
-ESSENTIAL_BREW_PACKAGES="coreutils diff-so-fancy fish node"
+# BREW (ESSENTIAL)
+ESSENTIAL_BREW_PACKAGES="coreutils diff-so-fancy fish node python"
 
-# FISH PACKAGES (installed with fisherman)
+# FISH (installed with fisherman)
 FISHERMAN_PACKAGES="pure z jbonjean/re-search"
+
+# BASH (installed with brew)
+BASH_BREW_PACKAGES="bash bash-completion z"
--- setup/bash.sh
+++ setup/bash.sh
@@ -2,17 +2,37 @@
 
 # SCRIPT_DIR == Same folder as the script is placed in
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPT_DIR}/../settings.sh"
 source "${SCRIPT_DIR}/utils.sh"
 
 create_symlinks() {
+    print_info "Creating symlinks to the bash dotfiles"
     ${SCRIPT_DIR}/symlink_dotfiles.sh "../dotfiles/bash/"
 }
+
+install_bash_packages() {
+    print_info "Installing brew packages needed by bash:"
+    print_list $@
+    brew install "$@"
+    print_success "Brew packages successfully installed\n"
+}
+
+add_bash_as_allowed_shell() {
+    print_info "Adding Bash 4 to list of allowed shells"
+    # Add bash to /etc/shells (the list of allowed shells)
+    # If "/usr/local/bin/fish" isn't already in "/etc/shells", add it to the list"
+    grep -q -F '/usr/local/bin/bash' '/etc/shells' || echo '/usr/local/bin/bash' | sudo tee -a '/etc/shells'
+    print_success "Success!"
+}
+
 
 print_heading "Bash"
 printf "If you want, we can make Bash a bit better as well. Fish will still be the default shell, so this will only be useful if you sometimes want to use bash instead of fish\n\n"
 
 if ask_question "Add Bash support?"; then
     create_symlinks
+    install_bash_packages $BASH_BREW_PACKAGES
+    add_bash_as_allowed_shell
 else
     print_error "Good riddance!"
 fi
